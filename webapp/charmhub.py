3f35dc039172c2be61ed9bacc7fc8a6cbbf4a779
Lukewh <luke@lukewh.com>
2020-03-25 21:14:33 +0000

    Make charmhub specific config, make app.py more generic

--- webapp/charmhub.py
+++ webapp/charmhub.py
@@ -1,12 +1,9 @@
-class Config:
+class Config():
     def __init__(self, d):
         self.__dict__ = d
 
-
-config = Config(
-    {
-        "app_name": "charmhub.io",
-        "details_regex": "[a-z0-9-]*[a-z][a-z0-9-]*",
-        "details_regex_uppercase": "[A-Za-z0-9-]*[A-Za-z][A-Za-z0-9-]*",
-    }
-)
+config = Config({
+    "app_name": "charmhub.io",
+    "details_regex": "[a-z0-9-]*[a-z][a-z0-9-]*",
+    "details_regex_uppercase": "[A-Za-z0-9-]*[A-Za-z][A-Za-z0-9-]*"
+})
