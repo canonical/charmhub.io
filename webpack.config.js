c36528fb512a942704de5c3bbc814d4615fe50d5
Steve Rydz <steve.rydz@canonical.com>
2020-06-05 14:13:13 +0100

    Use correct webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
 
+const entry = require("./.webpack/entry.js");
+const rules = require("./.webpack/rules.js");
+
 const TerserPlugin = require("terser-webpack-plugin");
 
 const production = process.env.ENVIRONMENT !== "devel";
@@ -14,13 +17,7 @@ const minimizer = production
   : [];
 
 module.exports = {
-  entry: {
-    "global-nav": "./static/js/base/global-nav.js",
-    about: "./static/js/public/about/index.js",
-    base: "./static/js/base/base.js",
-    details: "./static/js/public/details/index.js",
-    store: "./static/js/public/store/index.js",
-  },
+  entry: entry,
   output: {
     filename: "[name].js",
     path: __dirname + "/static/js/dist",
@@ -28,37 +25,7 @@ module.exports = {
   mode: production ? "production" : "development",
   devtool: production ? "source-map" : "eval-source-map",
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // Exclude node_modules from using babel-loader
-        // except some that use ES6 modules and need to be transpiled:
-        // such as swiper http://idangero.us/swiper/get-started/
-        // and also react-dnd related
-        exclude: /node_modules\/(?!(dom7|ssr-window)\/).*/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      // loaders are evaluated from bottom to top (right to left)
-      // so first transpile via babel, then expose as global
-      {
-        test: require.resolve(__dirname + "/static/js/public/about/index.js"),
-        use: ["expose-loader?charmhub.about", "babel-loader"],
-      },
-      {
-        test: require.resolve(__dirname + "/static/js/base/base.js"),
-        use: ["expose-loader?charmhub.base", "babel-loader"],
-      },
-      {
-        test: require.resolve(__dirname + "/static/js/public/store/index.js"),
-        use: ["expose-loader?charmhub.store", "babel-loader"],
-      },
-      {
-        test: require.resolve(__dirname + "/static/js/public/details/index.js"),
-        use: ["expose-loader?charmhub.details", "babel-loader"],
-      },
-    ],
+    rules: rules,
   },
   optimization: {
     minimize: true,
