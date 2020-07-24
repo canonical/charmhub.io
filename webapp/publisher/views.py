08a7ef1e6b53b24cc693c006ef526ce6cc83bb7e
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-07-24 08:16:30 +0100

    Get publisher charms from API

--- webapp/publisher/views.py
+++ webapp/publisher/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
-from flask import render_template
+import talisker
+from flask import Blueprint, render_template, session
+
+from canonicalwebteam.store_api.stores.charmstore import CharmPublisher
 
 from webapp.config import DETAILS_VIEW_REGEX
 from webapp.decorators import login_required
@@ -10,20 +12,34 @@ publisher = Blueprint(
     template_folder="/templates",
     static_folder="/static",
 )
+publisher_api = CharmPublisher(talisker.requests.get_session())
 
 
 @publisher.route("/account/details")
 @login_required
 def get_account_details():
-
     return render_template("publisher/account-details.html")
 
 
-@publisher.route("/charms-and-bundles")
+@publisher.route("/charms")
 @login_required
-def charms_and_bundles():
+def charms():
+    context = {
+        "published": publisher_api.get_account_packages(
+            session["publisher-auth"], "charm", "published"
+        ),
+        "registered": publisher_api.get_account_packages(
+            session["publisher-auth"], "charm", "registered"
+        ),
+    }
 
-    return render_template("publisher/charms-and-bundles.html")
+    return render_template("publisher/charms.html", **context)
+
+
+@publisher.route("/bundles")
+@login_required
+def bundles():
+    return render_template("publisher/bundles.html")
 
 
 @publisher.route('/<regex("' + DETAILS_VIEW_REGEX + '"):entity_name>/listing')
