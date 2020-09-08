f893e8dbd36a9d4c72fc04ad77547f2eecfecc09
Toto Bille <thomas.bille@canonical.com>
2020-09-08 15:02:26 +0100

    Add SSO everywhere (#267)
    * Add SSO everywhere
    
    * Fix lint
    
    * Remove 404 tests since now login everywhere
    
    * Add index and static endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,16 +18,6 @@ class TestRoutes(unittest.TestCase):
 
         self.assertEqual(self.client.get("/").status_code, 200)
 
-    def test_not_found(self):
-        """
-        When given a non-existent URL,
-        we should return a 404 status code
-        """
-
-        self.assertEqual(
-            self.client.get("/anystring/not-found-url").status_code, 404
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
