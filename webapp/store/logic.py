0b1b0bf1dbb7a318d6527394f11cc0bb4c560bf5
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-09-21 14:46:48 +0100

    Channel map reshape

--- webapp/store/logic.py
+++ webapp/store/logic.py
@@ -26,15 +26,15 @@ def convert_channel_maps(channel_map):
     """
     Converts channel maps list to format easier to manipulate
 
-    :param channel_maps_list: The channel maps list returned by the API
+    :param channel_maps: The channel maps list returned by the API
 
     :returns: The channel maps reshaped
     """
     channel_map_restruct = {}
 
     for channel in channel_map:
-        track = channel.get("channel").get("track")
-        risk = channel.get("channel").get("risk")
+        track = channel["channel"].get("track", "latest")
+        risk = channel["channel"]["risk"]
 
         if track not in channel_map_restruct:
             channel_map_restruct[track] = {}
@@ -43,11 +43,12 @@ def convert_channel_maps(channel_map):
             channel_map_restruct[track][risk] = []
 
         info = {
-            "created_at": convert_date(channel["revision"]["created-at"]),
+            "released_at": convert_date(channel["channel"]["released-at"]),
             "version": channel["revision"]["version"],
             "channel": channel["channel"]["name"],
             "risk": channel["channel"]["risk"],
             "size": channel["revision"]["download"]["size"],
+            "platform": channel["channel"]["platform"]["series"],
         }
 
         channel_map_restruct[track][risk].append(info)
@@ -123,11 +124,15 @@ def add_store_front_data(package):
     tags = extra["metadata"].get("tags")
     extra["categories"] = get_categories(tags) if tags else []
 
+    # Reshape channel maps
+    extra["channel_map"] = convert_channel_maps(package["channel-map"])
+
+    # Some needed fields
     extra["publisher_name"] = package["result"]["publisher"]["display-name"]
     extra["last_release"] = convert_date(
         package["default-release"]["channel"]["released-at"]
     )
     extra["summary"] = package["result"]["summary"]
-    package["store_front"] = extra
 
+    package["store_front"] = extra
     return package
