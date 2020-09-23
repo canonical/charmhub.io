65e0248b1004577a135cf23468e5cfe17799d861
Toto Bille <thomas.bille@canonical.com>
2020-09-23 11:31:19 +0100

    Update configure tab name (#386)
    * Update configure tab name
    
    * Rename configuration to configure
    
    * Fix lint

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -180,14 +180,12 @@ def details_docs(entity_name, slug=None):
     return render_template("details/docs.html", **context)
 
 
-@store.route(
-    '/<regex("' + DETAILS_VIEW_REGEX + '"):entity_name>/configuration'
-)
+@store.route('/<regex("' + DETAILS_VIEW_REGEX + '"):entity_name>/configure')
 def details_configuration(entity_name):
     package = app.store_api.get_item_details(entity_name, fields=FIELDS)
     package = logic.add_store_front_data(package)
 
-    return render_template("details/configuration.html", package=package)
+    return render_template("details/configure.html", package=package)
 
 
 @store.route('/<regex("' + DETAILS_VIEW_REGEX + '"):entity_name>/history')
