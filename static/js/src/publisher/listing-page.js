77c77eb8418098cdcebecb6b6f1822c988d61e85
solazio <ovidiu.suciu@canonical.com>
2020-12-02 13:03:39 +0000

    Update publisher listing page

--- static/js/src/publisher/listing-page.js
+++ static/js/src/publisher/listing-page.js
@@ -21,6 +21,25 @@ function initTooltips() {
   return this;
 }
 
+function initStickyMenu(selector) {
+  const observerEl = document.querySelector(selector);
+
+  if (observerEl) {
+    const stickyEl = observerEl.querySelector("[data-js='sticky-menu']");
+    let observer = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio > 0) {
+        stickyEl.classList.remove("is-sticky");
+      } else {
+        stickyEl.classList.add("is-sticky");
+      }
+    });
+
+    observer.observe(observerEl);
+  } else {
+    throw new Error(`There is no element containing ${selector} selector.`);
+  }
+}
+
 function initLicenses(inputs) {
   function licenseTypeChange() {
     var type = this.value;
@@ -41,4 +60,4 @@ function initLicenses(inputs) {
   }
 }
 
-export { initTooltips, initMultiselect, initLicenses };
+export { initTooltips, initMultiselect, initLicenses, initStickyMenu };
