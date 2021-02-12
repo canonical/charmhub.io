af272267bc1fb5d72dd9b15a8abf30d39ab5948a
Ovidiu Suciu <40214246+solazio@users.noreply.github.com>
2021-02-12 11:12:21 +0200

    Fix info tooltip on mobile (#807)

--- static/js/src/base/base.js
+++ static/js/src/base/base.js
@@ -2,5 +2,6 @@ import { cookiePolicy } from "@canonical/cookie-policy";
 import "./polyfills";
 import "./navigation";
 import "./dynamic-contact-form";
+import "./tooltip-icon-modal";
 
 cookiePolicy();
