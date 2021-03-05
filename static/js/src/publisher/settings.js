dd91ac7a7233269bdd5ab51aa1c8768d4991a1e8
solazio <ovidiu.suciu@canonical.com>
2021-03-05 14:54:37 +0000

    Update settings page with the latest design

--- static/js/src/publisher/settings.js
+++ static/js/src/publisher/settings.js
@@ -1,52 +1,20 @@
-import { initMultiselect } from "./components/multiselect";
+function initStickyMenu(selector) {
+  const observerEl = document.querySelector(selector);
 
-function enableInput(type) {
-  const blackListMultiselect = document.querySelector(
-    ".js-blacklist .p-multiselect"
-  );
-  const blackListMultiselectInput = blackListMultiselect.querySelector(
-    ".p-multiselect__input"
-  );
-  const whiteListMultiselect = document.querySelector(
-    ".js-whitelist .p-multiselect"
-  );
-  const whiteListMultiselectInput = whiteListMultiselect.querySelector(
-    ".p-multiselect__input"
-  );
+  if (observerEl) {
+    const stickyEl = observerEl.querySelector("[data-js='sticky-menu']");
+    let observer = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio > 0) {
+        stickyEl.classList.remove("is-sticky");
+      } else {
+        stickyEl.classList.add("is-sticky");
+      }
+    });
 
-  if (type === "whitelist") {
-    blackListMultiselect.classList.add("is-disabled");
-    blackListMultiselectInput.setAttribute("disabled", "disabled");
-    whiteListMultiselectInput.removeAttribute("disabled");
-    whiteListMultiselect.classList.remove("is-disabled");
-  } else if (type === "blacklist") {
-    whiteListMultiselect.classList.add("is-disabled");
-    whiteListMultiselectInput.setAttribute("disabled", "disabled");
-    blackListMultiselectInput.removeAttribute("disabled");
-    blackListMultiselect.classList.remove("is-disabled");
+    observer.observe(observerEl);
+  } else {
+    throw new Error(`There is no element containing ${selector} selector.`);
   }
 }
 
-function changeHandler(e) {
-  const target = e.target;
-
-  if (target.id === "territories_custom" || target.id === "territories_all") {
-    const custom = document.getElementById("territories_custom");
-    const checked = custom.checked;
-    const nested = custom.parentNode.querySelector(
-      "[data-js='custom-territories-holder']"
-    );
-
-    if (checked) {
-      nested.classList.remove("u-hide");
-    } else {
-      nested.classList.add("u-hide");
-    }
-  } else if (target.id === "territories_custom_whitelist" && target.checked) {
-    enableInput("whitelist");
-  } else if (target.id === "territories_custom_blacklist" && target.checked) {
-    enableInput("blacklist");
-  }
-}
-
-export { initMultiselect, enableInput, changeHandler };
+export { initStickyMenu };
