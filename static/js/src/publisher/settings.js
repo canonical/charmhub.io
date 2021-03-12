262b93186962929da231b90369cb99b8fdd2b151
solazio <ovidiu.suciu@canonical.com>
2021-03-12 10:04:05 +0000

    Connect settings page to the api

--- static/js/src/publisher/settings.js
+++ static/js/src/publisher/settings.js
@@ -1,20 +1,80 @@
-function initStickyMenu(selector) {
-  const observerEl = document.querySelector(selector);
+const ALLOWED_KEYS = ["private"];
 
-  if (observerEl) {
-    const stickyEl = observerEl.querySelector("[data-js='sticky-menu']");
-    let observer = new IntersectionObserver((entries) => {
-      if (entries[0].intersectionRatio > 0) {
-        stickyEl.classList.remove("is-sticky");
-      } else {
-        stickyEl.classList.add("is-sticky");
-      }
+class SettingsForm {
+  constructor(selector, initialState) {
+    this.formEl = document.querySelector(selector);
+
+    if (this.formEl) {
+      this.validation = {};
+
+      this.initialState = JSON.parse(JSON.stringify(initialState));
+      this.currentState = JSON.parse(JSON.stringify(initialState));
+
+      this.init();
+    } else {
+      throw new Error(`${selector} is not a valid element`);
+    }
+  }
+
+  init() {
+    this.initStickyMenu("[data-js='sticky-menu-observer']");
+
+    // validate inputs on change
+    this.formEl.addEventListener("input", (event) => {
+      this.updateCurrentState(event.target);
     });
+  }
 
-    observer.observe(observerEl);
-  } else {
-    throw new Error(`There is no element containing ${selector} selector.`);
+  diffState() {
+    const diff = {};
+
+    for (let key of ALLOWED_KEYS) {
+      if (this.initialState[key] !== this.currentState[key]) {
+        diff[key] = this.currentState[key];
+      }
+    }
+
+    // only return diff when there are any changes
+    return Object.keys(diff).length > 0 ? diff : null;
+  }
+
+  updateCurrentState(target) {
+    const key = target.getAttribute("name");
+    if (key && ALLOWED_KEYS.includes(key)) {
+      this.currentState[key] = target.value === "private" ? true : false;
+    }
+
+    const diff = this.diffState();
+
+    if (diff) {
+      this.stickyEl.classList.remove("u-hide");
+    } else {
+      this.stickyEl.classList.add("u-hide");
+    }
+  }
+
+  initStickyMenu(selector) {
+    this.observerEl = document.querySelector(selector);
+
+    if (this.observerEl) {
+      this.stickyEl = this.observerEl.querySelector("[data-js='sticky-menu']");
+      let observer = new IntersectionObserver((entries) => {
+        if (entries[0].intersectionRatio > 0) {
+          this.stickyEl.classList.remove("is-sticky");
+        } else {
+          this.stickyEl.classList.add("is-sticky");
+        }
+      });
+
+      observer.observe(this.observerEl);
+    } else {
+      throw new Error(`There is no element containing ${selector} selector.`);
+    }
   }
 }
 
-export { initStickyMenu };
+function init(initialState) {
+  new SettingsForm("#settings-form", initialState);
+}
+
+export { init };
