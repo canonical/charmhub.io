1de6f7026c9d4b4ffffa1a2ede9037af9afc526e
cmisare-canonical <84032480+cmisare-canonical@users.noreply.github.com>
2021-06-01 17:30:58 -0600

    add badges for redis-k8s charm (#1028)

--- webapp/store/badges.py
+++ webapp/store/badges.py
@@ -206,4 +206,13 @@ OPS_BADGES = {
         "unit-testing": True,
         "high-availability": False,
     },
+    "redis-k8s": {
+        "detailed-documentation": True,
+        "guaranteed-getting-started": True,
+        "secrets-management": True,
+        "implements-sidecar-pattern": False,
+        "seamless-upgrades": False,
+        "unit-testing": True,
+        "high-availability": False,
+    },
 }
