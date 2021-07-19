e4167bedc48f31c35c0706c0f9800023968a5404
Steve Rydz <steve.rydz@canonical.com>
2021-07-19 10:45:10 +0100

    Import bash language module for hljs

--- static/js/src/base/highlight.js
+++ static/js/src/base/highlight.js
@@ -1,7 +1,9 @@
 import hljs from "highlight.js/lib/core";
 import python from "highlight.js/lib/languages/python";
+import bash from "highlight.js/lib/languages/bash";
 import "highlight.js/styles/github.css";
 
 hljs.registerLanguage("python", python);
+hljs.registerLanguage("bash", bash);
 
 export { hljs };
