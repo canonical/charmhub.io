5775b53eae270879eae2f7dac7b9512d973feb80
Francisco Jiménez Cabrera <francisco.jimenez.cabrera@canonical.com>
2022-02-11 15:03:17 +0000

    Topics page (#1252)
    * JS fix: Limit results by 3
    
    * Changes to include topics page

--- webapp/config.py
+++ webapp/config.py
@@ -1,2 +1,15 @@
 APP_NAME = "charmhub.io"
 DETAILS_VIEW_REGEX = "[A-Za-z0-9-]*[A-Za-z][A-Za-z0-9-]*"
+
+CATEGORIES = [
+    {"slug": "ai-ml", "name": "AI/ML"},
+    {"slug": "big-data", "name": "Big Data"},
+    {"slug": "cloud", "name": "Cloud"},
+    {"slug": "containers", "name": "Containers"},
+    {"slug": "databases", "name": "Databases"},
+    {"slug": "logging-tracing", "name": "Logging and Tracing"},
+    {"slug": "monitoring", "name": "Monitoring"},
+    {"slug": "networking", "name": "Networking"},
+    {"slug": "security", "name": "Security"},
+    {"slug": "storage", "name": "Storage"},
+]
