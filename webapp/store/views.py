4bc50b67282e25fcba765f334e72a6c0b2d13a75
Lukewh <luke@lukewh.com>
2022-04-19 12:34:00 +0100

    Fix python linting

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -560,8 +560,10 @@ def entity_embedded_card(entity_name):
     try:
         package = get_package(entity_name, channel_request, FIELDS)
 
-        package["default-release"]["channel"]["released-at"] = logic.convert_date(
-            package["default-release"]["channel"]["released-at"]
+        package["default-release"]["channel"]["released-at"] = (
+            logic.convert_date(
+                package["default-release"]["channel"]["released-at"]
+            )
         )
 
         button = request.args.get("button")
