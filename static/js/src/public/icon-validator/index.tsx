a469992a8d8e95c1dab6d234e3a73644be3bc390
Lukewh <luke@lukewh.com>
2022-09-23 15:44:06 +0100

    Icon validation page
    Add icon-validator file to tsconfig
    
    reinstall deps
    
    More specific include path
    
    Fix issues?
    
    Still trying to fix things
    
    Add explicit rule
    
    Remove explicit rule

--- static/js/src/public/icon-validator/index.tsx
+++ static/js/src/public/icon-validator/index.tsx
@@ -1,33 +1,52 @@
-import type { ChangeEvent } from "react";
-import { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import ReactDOM from "react-dom";
+import {
+  Form,
+  Input,
+  Row,
+  Col,
+  Icon,
+  Notification,
+  ICONS,
+} from "@canonical/react-components";
+
+import { useDropzone } from "react-dropzone";
 
 function App() {
   const [icon, setIcon] = useState<string | null>(null);
   const [isImage, setIsImage] = useState<boolean>(false);
   const [isSVG, setIsSVG] = useState<boolean>(false);
   const [isCorrectSize, setIsCorrectSize] = useState<boolean>(false);
-  const [hasCircleBackground, setHasCircleBackground] = useState<boolean>(
-    false
-  );
-  const [hasSolidBackground, setHasSolidBackground] = useState<boolean>(false);
+  const [imageSize, setImageSize] = useState<number[]>([100, 100]);
+
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragAccept,
+  } = useDropzone({
+    maxFiles: 1,
+  });
 
   const handleReset = useCallback(() => {
     setIcon(null);
     setIsImage(false);
     setIsSVG(false);
     setIsCorrectSize(false);
-    setHasCircleBackground(false);
-    setHasSolidBackground(false);
   }, []);
 
-  const handleImageChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    handleReset();
-    if (!e.target.files || e.target.files.length === 0) {
+  const isValid = useMemo(() => isImage && isSVG && isCorrectSize, [
+    isImage,
+    isSVG,
+    isCorrectSize,
+  ]);
+
+  useEffect(() => {
+    if (!acceptedFiles.length) {
       return;
     }
-
-    const file = e.target.files[0];
+    handleReset();
+    const file = acceptedFiles[0];
 
     if (!file.type.startsWith("image/")) {
       return;
@@ -40,7 +59,7 @@ function App() {
     }
 
     const urlReader = new FileReader();
-    urlReader.addEventListener("load", (e: any) => {
+    urlReader.addEventListener("load", (e) => {
       if (!e.target || !e.target.result) {
         setIsImage(false);
         return;
@@ -48,36 +67,110 @@ function App() {
 
       const img = new Image();
       img.addEventListener("load", () => {
-        setIcon(e.target.result);
-        if (img.width !== 100 || img.height !== 100) {
-          setIsCorrectSize(false);
-        } else {
-          setIsCorrectSize(true);
+        if (e?.target?.result) {
+          setIcon(e?.target?.result);
+          setImageSize([img.width, img.height]);
+          if (img.width !== 100 || img.height !== 100) {
+            setIsCorrectSize(false);
+          } else {
+            setIsCorrectSize(true);
+          }
         }
       });
       img.src = e.target.result;
     });
     urlReader.readAsDataURL(file);
-  }, []);
+  }, [acceptedFiles, handleReset]);
 
   return (
-    <div>
-      <button onClick={handleReset}>Reset</button>
-      <input type="file" onChange={handleImageChange} />
-      {icon && (
-        <>
-          <img src={icon} />
-          <p>Is image: {isImage ? "true" : "false"}</p>
-          <p>Is SVG: {isSVG ? "true" : "false"}</p>
-          <p>Is correct size: {isCorrectSize ? "true" : "false"}</p>
-          <p>
-            <b>Please check that:</b>
-            <br />
-            The icon has a cirular solid background.
-          </p>
-        </>
-      )}
-    </div>
+    <Row>
+      <Col size={8} emptyLarge={4}>
+        <h1>Charm Icon Validator</h1>
+        <p>
+          Checks are based on the{" "}
+          <a href="https://juju.is/docs/sdk/create-an-icon-for-your-charm">
+            Charm Icon Specification
+          </a>
+          .
+        </p>
+        <p>Drag and drop, or use the file picker to choose your icon.</p>
+        <Form stacked={true} style={{ marginBottom: "1rem" }}>
+          <div
+            {...getRootProps({ className: "dropzone" })}
+            style={{
+              outline: isDragAccept
+                ? "3px dashed #0e8420"
+                : "3px solid transparent",
+              borderRadius: "3px",
+              display: "block",
+            }}
+          >
+            <Input type="file" />
+            <input {...getInputProps()} />
+            {icon && (
+              <>
+                <Row>
+                  <Col size={2}>
+                    <img src={icon} style={{ maxWidth: "100%" }} />
+                  </Col>
+                  <Col size={6}>
+                    <p>
+                      <Icon name={isImage ? ICONS.success : ICONS.error} />{" "}
+                      Image
+                    </p>
+                    <p>
+                      <Icon name={isSVG ? ICONS.success : ICONS.error} /> SVG
+                    </p>
+                    <p>
+                      <Icon
+                        name={
+                          imageSize[0] === 100 ? ICONS.success : ICONS.error
+                        }
+                      />{" "}
+                      Image width is {imageSize[0]}px, not 100px
+                    </p>
+                    <p>
+                      <Icon
+                        name={
+                          imageSize[1] === 100 ? ICONS.success : ICONS.error
+                        }
+                      />{" "}
+                      Image height is {imageSize[1]}px, not 100px
+                    </p>
+                  </Col>
+                </Row>
+              </>
+            )}
+          </div>
+        </Form>
+        {icon && (
+          <Notification
+            severity={isValid ? "positive" : "negative"}
+            title={isValid ? "Valid icon" : "Invalid icon"}
+          >
+            {isValid ? (
+              <>
+                Based on the automated checks above your icon is valid, but you
+                should manually check the{" "}
+                <a href="https://juju.is/docs/sdk/create-an-icon-for-your-charm">
+                  Charm Icon Specification
+                </a>
+                .
+              </>
+            ) : (
+              <>
+                Based on the automated checks above your icon is not valid.
+                Please check the{" "}
+                <a href="https://juju.is/docs/sdk/create-an-icon-for-your-charm">
+                  Charm Icon Specification
+                </a>
+                , update your icon, and try again.
+              </>
+            )}
+          </Notification>
+        )}
+      </Col>
+    </Row>
   );
 }
 
