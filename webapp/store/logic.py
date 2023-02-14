b7dadba13885ef42c72c9f4a87230f9ff31c307e
codeEmpress1 <alimot.akinbode@canonical.com>
2023-02-14 13:41:03 +0300

    fix duplicate platforms (#1489)
    * fix duplicate platforms
    
    * add comment

--- webapp/store/logic.py
+++ webapp/store/logic.py
@@ -165,25 +165,31 @@ def extract_default_release_architectures(channel):
 def extract_all_arch(channel_map, parent_dict):
     all_archy = set()
     all_channel_bases = []
-    for version, version_data in channel_map.items():
-        for channel, channel_data in version_data.items():
-            bases = set()
-            name = ""
-            for _, release in channel_data["releases"].items():
-                all_archy = all_archy.union(release["architectures"])
-                bases = bases.union(release["bases"])
 
-            if channel_data["latest"]["channel_bases"]:
-                for base in channel_data["latest"]["channel_bases"]:
-                    name = base["name"]
+    if channel_map["latest"]:
+        channel_map_latest = channel_map["latest"]
+    # for charms without the latest revision
+    else:
+        channel_map_latest = channel_map[0]
 
-            bases = sorted(
-                bases, key=lambda k: k.replace("Ubuntu ", ""), reverse=True
-            )
+    for channel, channel_data in channel_map_latest.items():
+        bases = set()
+        name = ""
+        for _, release in channel_data["releases"].items():
+            all_archy = all_archy.union(release["architectures"])
+            bases = bases.union(release["bases"])
 
-            all_channel_bases.append(
-                {"channel": channel, "bases": bases, "name": name}
-            )
+        if channel_data["latest"]["channel_bases"]:
+            for base in channel_data["latest"]["channel_bases"]:
+                name = base["name"]
+
+        bases = sorted(
+            bases, key=lambda k: k.replace("Ubuntu ", ""), reverse=True
+        )
+
+        all_channel_bases.append(
+            {"channel": channel, "bases": bases, "name": name}
+        )
 
     parent_dict["all_architectures"] = all_archy
     parent_dict["all_channel_bases"] = all_channel_bases
