3d1f37c587001a16000d96c92ce011632612653b
Steve Rydz <steve.rydz@canonical.com>
2023-03-22 13:52:11 +0000

    Fixing filters

--- static/js/beta/store/components/Packages/Packages.tsx
+++ static/js/beta/store/components/Packages/Packages.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { Strip, Row } from "@canonical/react-components";
-import { CardList } from "@canonical/store-components";
+import { Strip, Row, Col } from "@canonical/react-components";
+import { CardList, Filters } from "@canonical/store-components";
 
 const getPackages = async () => {
   const response = await fetch("/packages.json");
@@ -34,7 +34,7 @@ const formatData = (data: any) => {
         name: d.result.publisher["display-name"],
         validation: null,
       },
-      categories: d.store_front["categories"],
+      categories: d.store_front["categories"] || [],
     };
   });
 };
@@ -43,15 +43,81 @@ function Packages() {
   const { data } = useQuery("packages", getPackages);
   const formattedData = formatData(data);
 
+  const [packages, setPackages] = useState(formattedData);
+
   return (
     <Strip>
-      {formattedData.length > 0 ? (
-        <CardList packages={formattedData} itemsPerPage={12} />
-      ) : (
-        <Row>
-          <h2>Loading packages...</h2>
-        </Row>
-      )}
+      <Row>
+        <Col size={3}>
+          {formattedData && formattedData.length > 0 && (
+            <Filters
+              setFilteredPackages={setPackages}
+              packages={formattedData}
+              categories={[
+                {
+                  display_name: "AI/ML",
+                  name: "ai-ml",
+                },
+                {
+                  display_name: "Big Data",
+                  name: "bigdata",
+                },
+                {
+                  display_name: "Cloud",
+                  name: "cloud",
+                },
+                {
+                  display_name: "Containers",
+                  name: "containers",
+                },
+                {
+                  display_name: "Databases",
+                  name: "databases",
+                },
+                {
+                  display_name: "Logging and Tracing",
+                  name: "logging-and-tracing",
+                },
+                {
+                  display_name: "Monitoring",
+                  name: "monitoring",
+                },
+                {
+                  display_name: "Networking",
+                  name: "networking",
+                },
+                {
+                  display_name: "Security",
+                  name: "security",
+                },
+                {
+                  display_name: "Storage",
+                  name: "storage",
+                },
+              ]}
+              types={[
+                { label: "All", value: "all" },
+                { label: "Charms", value: "charm" },
+                { label: "Bundles", value: "bundle" },
+              ]}
+              platforms={[
+                { label: "All", value: "all" },
+                { label: "VM", value: "vm" },
+                { label: "Kubernetes", value: "kubernetes" },
+              ]}
+            />
+          )}
+        </Col>
+        <Col size={9}>
+          {formattedData.length > 0 ? (
+            <CardList packages={formattedData} itemsPerPage={12} />
+          ) : (
+            <Row>
+              <h2>Loading packages...</h2>
+            </Row>
+          )}
+        </Col>
+      </Row>
     </Strip>
   );
 }
