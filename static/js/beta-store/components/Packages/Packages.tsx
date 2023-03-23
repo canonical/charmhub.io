c3fcab9a9f43258835c42f6ebb4d3ecb24cdf86e
Steve Rydz <steve.rydz@canonical.com>
2023-03-23 16:52:29 +0000

    Add packages page to beta store

---
+++ static/js/beta-store/components/Packages/Packages.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { Strip, Row, Col } from "@canonical/react-components";
+import { PackageCard } from "@canonical/store-components";
+
+function Packages() {
+  const getPackages = async () => {
+    const response = await fetch("/beta/store");
+    const packages = await response.json();
+    const packagesWithId = packages.packages.map((item: any) => {
+      return {
+        ...item,
+        id: crypto.randomUUID(),
+      };
+    });
+
+    return packagesWithId;
+  };
+
+  const { data, status } = useQuery("packages", getPackages);
+
+  return (
+    <Strip>
+      <Row>
+        <Col size={3}>
+          <h2 className="p-heading--4">Filters</h2>
+        </Col>
+        <Col size={9}>
+          <Row>
+            {status === "loading" && <p>Loading packages...</p>}
+
+            {status === "success" &&
+              data.length > 0 &&
+              data.map((packageData: any) => (
+                <Col size={3}>
+                  <PackageCard key={packageData.id} item={packageData} />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+      </Row>
+    </Strip>
+  );
+}
+
+export default Packages;
