a774d7b5e905a4ed8f020c2ca9a44a4525127b1b
Steve Rydz <steve.rydz@canonical.com>
2023-03-31 10:59:52 +0100

    Add pagination to beta store page

--- static/js/beta-store/components/Packages/Packages.tsx
+++ static/js/beta-store/components/Packages/Packages.tsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
-import { Strip, Row, Col } from "@canonical/react-components";
+import { useLocation } from "react-router-dom";
+import { Strip, Row, Col, Pagination } from "@canonical/react-components";
 import { CharmCard } from "@canonical/store-components";
 
 function Packages() {
-  const getPackages = async () => {
-    const response = await fetch("/beta/store");
-    const packages = await response.json();
-    const packagesWithId = packages.packages.map((item: any) => {
+  const getData = async () => {
+    const response = await fetch(`/beta/store?page=${currentPage}`);
+    const data = await response.json();
+    const packagesWithId = data.packages.map((item: any) => {
       return {
         ...item,
         id: crypto.randomUUID(),
       };
     });
 
-    return packagesWithId;
+    return {
+      total_pages: data.total_pages,
+      packages: packagesWithId,
+    };
   };
 
-  const { data, status } = useQuery("packages", getPackages);
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+  const [currentPage, setCurrentPage] = useState(
+    queryParams.get("page") || "1"
+  );
+  const { data, status, refetch } = useQuery("data", getData);
+
+  useEffect(() => {
+    refetch();
+  }, [currentPage]);
 
   return (
     <Strip>
@@ -30,13 +43,29 @@ function Packages() {
             {status === "loading" && <p>Loading packages...</p>}
 
             {status === "success" &&
-              data.length > 0 &&
-              data.map((packageData: any) => (
-                <Col size={3} style={{ marginBottom: "1.5rem" }}>
-                  <CharmCard key={packageData.id} data={packageData} />
+              data.packages.length > 0 &&
+              data.packages.map((packageData: any) => (
+                <Col
+                  size={3}
+                  style={{ marginBottom: "1.5rem" }}
+                  key={packageData.id}
+                >
+                  <CharmCard data={packageData} />
                 </Col>
               ))}
           </Row>
+          <Pagination
+            itemsPerPage={12}
+            totalItems={data?.total_pages}
+            paginate={(pageNumber) => {
+              setCurrentPage(pageNumber.toString());
+              queryParams.set("page", pageNumber.toString());
+              window.location.search = queryParams.toString();
+            }}
+            currentPage={parseInt(currentPage)}
+            centered
+            scrollToTop
+          />
         </Col>
       </Row>
     </Strip>
