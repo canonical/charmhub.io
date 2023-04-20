113756c58911f3d664698f756a52f55391946f0b
Lukewh <luke@lukewh.com>
2023-04-20 09:57:46 +0100

    Black

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -160,7 +160,9 @@ def details_overview(entity_name):
 
     if not package["store_front"]["docs_topic"]:
         navigation = None
-        description = package["store_front"]["metadata"].get("description", None)
+        description = package["store_front"]["metadata"].get(
+            "description", None
+        )
         summary = package["store_front"]["metadata"].get("summary", None)
     else:
         docs_url_prefix = f"/{package['name']}/docs"
