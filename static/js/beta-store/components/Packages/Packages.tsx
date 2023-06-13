d5ceda4f125bf7746ab8319522f5ee9145ea8f5f
Steve Rydz <steve.rydz@canonical.com>
2023-06-13 16:42:57 +0100

    Add filters to beta store page (#1606)
    Bump store base
    
    Make Filters component
    
    Update filters component
    
    Update logic in Filters
    
    Disable filters until data is ready
    
    Fetch data
    
    Fix filters
    
    Add platforms
    
    Add package type filters
    
    Update filters
     Please enter the commit message for your changes. Lines starting
    
    Use store component filters

--- static/js/beta-store/components/Packages/Packages.tsx
+++ static/js/beta-store/components/Packages/Packages.tsx
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 import { Strip, Row, Col, Pagination } from "@canonical/react-components";
-import { CharmCard } from "@canonical/store-components";
+import { CharmCard, Filters } from "@canonical/store-components";
+
+import categories from "../../data/categories";
+import platforms from "../../data/platforms";
+import packageTypes from "../../data/package-types";
 
 function Packages() {
+  const getUpdatedSearchParams = (
+    page: number,
+    selectedCategories: Array<string>,
+    selectedPlatform: string,
+    selectedPackageType: string
+  ) => {
+    const searchParams: any = {};
+
+    if (page && page > 1) {
+      searchParams.page = page.toString();
+    }
+
+    if (selectedCategories && selectedCategories.length > 0) {
+      searchParams.categories = selectedCategories.join(",");
+    }
+
+    if (selectedPlatform) {
+      searchParams.platforms = selectedPlatform;
+    }
+
+    if (selectedPackageType) {
+      searchParams.type = selectedPackageType;
+    }
+
+    return searchParams;
+  };
+
   const getData = async () => {
-    const response = await fetch(`/beta/store?page=${currentPage}`);
+    const response = await fetch(`/beta/store.json${search}`);
     const data = await response.json();
     const packagesWithId = data.packages.map((item: any) => {
       return {
@@ -18,25 +49,87 @@ function Packages() {
     return {
       total_pages: data.total_pages,
       packages: packagesWithId,
+      categories: data.categories,
     };
   };
 
   const { search } = useLocation();
-  const queryParams = new URLSearchParams(search);
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const [currentPage, setCurrentPage] = useState(
-    queryParams.get("page") || "1"
+    searchParams.get("page") || "1"
   );
+
+  const [selectedCategories, setSelectedCategories] = useState(
+    searchParams.get("categories")?.split(",") || []
+  );
+
+  const [selectedPlatform, setSelectedPlatform] = useState(
+    searchParams.get("platforms") || ""
+  );
+
+  const [selectedPackageType, setSelectedPackageType] = useState(
+    searchParams.get("type") || ""
+  );
+
   const { data, status, refetch } = useQuery("data", getData);
 
   useEffect(() => {
     refetch();
-  }, [currentPage]);
+  }, [searchParams]);
+
+  useEffect(() => {
+    setCurrentPage("1");
+    setSearchParams(
+      getUpdatedSearchParams(
+        1,
+        selectedCategories,
+        selectedPlatform,
+        selectedPackageType
+      )
+    );
+  }, [selectedCategories]);
+
+  useEffect(() => {
+    setCurrentPage("1");
+    setSearchParams(
+      getUpdatedSearchParams(
+        1,
+        selectedCategories,
+        selectedPlatform,
+        selectedPackageType
+      )
+    );
+  }, [selectedPlatform]);
+
+  useEffect(() => {
+    setCurrentPage("1");
+    setSearchParams(
+      getUpdatedSearchParams(
+        1,
+        selectedCategories,
+        selectedPlatform,
+        selectedPackageType
+      )
+    );
+  }, [selectedPackageType]);
 
   return (
     <Strip>
       <Row>
         <Col size={3}>
-          <h2 className="p-heading--4">Filters</h2>
+          <Filters
+            categories={categories}
+            selectedCategories={selectedCategories}
+            setSelectedCategories={setSelectedCategories}
+            platforms={platforms}
+            selectedPlatform={selectedPlatform}
+            setSelectedPlatform={setSelectedPlatform}
+            packageTypes={packageTypes}
+            selectedPackageType={selectedPackageType}
+            setSelectedPackageType={setSelectedPackageType}
+            disabled={status === "loading"}
+          />
         </Col>
         <Col size={9}>
           <Row>
@@ -53,19 +146,39 @@ function Packages() {
                   <CharmCard data={packageData} />
                 </Col>
               ))}
+
+            {status === "success" && data.packages.length === 0 && (
+              <h1 className="p-heading--2">
+                No packages match{" "}
+                {`${
+                  selectedCategories.length > 1
+                    ? "these filters"
+                    : "this filter"
+                }`}
+              </h1>
+            )}
           </Row>
-          <Pagination
-            itemsPerPage={12}
-            totalItems={data?.total_pages}
-            paginate={(pageNumber) => {
-              setCurrentPage(pageNumber.toString());
-              queryParams.set("page", pageNumber.toString());
-              window.location.search = queryParams.toString();
-            }}
-            currentPage={parseInt(currentPage)}
-            centered
-            scrollToTop
-          />
+
+          {status === "success" && data.packages.length > 0 && (
+            <Pagination
+              itemsPerPage={12}
+              totalItems={data.total_pages}
+              paginate={(pageNumber) => {
+                setCurrentPage(pageNumber.toString());
+                setSearchParams(
+                  getUpdatedSearchParams(
+                    pageNumber,
+                    selectedCategories,
+                    selectedPlatform,
+                    selectedPackageType
+                  )
+                );
+              }}
+              currentPage={parseInt(currentPage)}
+              centered
+              scrollToTop
+            />
+          )}
         </Col>
       </Row>
     </Strip>
---
+++ static/js/beta-store/data/platforms.js
@@ -0,0 +1,7 @@
+const platforms = [
+  { display_name: "All", name: "all" },
+  { display_name: "VM", name: "vm" },
+  { display_name: "Kubernetes", name: "kubernetes" },
+];
+
+export default platforms;
