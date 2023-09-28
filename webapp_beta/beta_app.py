9a8378fbf88e164da95f0481f6621cff2f881141
Steve Rydz <steve.rydz@canonical.com>
2023-09-28 13:53:22 +0100

    Add beta store search heading (#1673)

--- webapp_beta/beta_app.py
+++ webapp_beta/beta_app.py
@@ -1,24 +1,28 @@
 import talisker.requests
 from canonicalwebteam.store_base.app import create_app
-from canonicalwebteam.store_api.stores.charmstore import CharmStore, CharmPublisher
+from canonicalwebteam.store_api.stores.charmstore import (
+    CharmStore,
+    CharmPublisher,
+)
 from canonicalwebteam.candid import CandidClient
 
 from webapp_beta.charmhub_bp import charmhub_bp
 from webapp.handlers import charmhub_utility_processor
+from webapp.decorators import login_required
 
 # beta charmhub configiuration
 app = create_app(
     "charmhub_beta",
+    login_required,
     store_bp=charmhub_bp,
     utility_processor=charmhub_utility_processor,
 )
 
 app.name = "charmhub_beta"
-app.static_folder=charmhub_bp.static_folder
-app.template_folder=charmhub_bp.template_folder
-app.static_url_path=charmhub_bp.static_url_path
+app.static_folder = charmhub_bp.static_folder
+app.template_folder = charmhub_bp.template_folder
+app.static_url_path = charmhub_bp.static_url_path
 app.store_api = CharmStore(session=talisker.requests.get_session())
-
 
 
 request_session = talisker.requests.get_session()
