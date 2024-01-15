1fa44d0a95d192dc48318fbbce876c3ec05d4269
Steve <steve.rydz@canonical.com>
2024-01-15 15:59:37 +0000

    Refactor collaborator invites

--- static/js/src/publisher/collaborator/components/App.tsx
+++ static/js/src/publisher/collaborator/components/App.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { RecoilRoot } from "recoil";
 
 import Collaborators from "./Collaborators";
 import InviteConfirmation from "./InviteConfirmation";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
+
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/:package_name/collaboration"
-          element={<Collaborators />}
-        />
-        <Route
-          path="/:package_name/collaboration/confirm"
-          element={<InviteConfirmation />}
-        />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route
+              path="/:packageName/collaboration"
+              element={<Collaborators />}
+            />
+            <Route
+              path="/:packageName/collaboration/confirm"
+              element={<InviteConfirmation />}
+            />
+          </Routes>
+        </QueryClientProvider>
+      </Router>
+    </RecoilRoot>
   );
 }
 
--- static/js/src/publisher/collaborator/components/Collaborators.tsx
+++ static/js/src/publisher/collaborator/components/Collaborators.tsx
@@ -1,4 +1,8 @@
-import React, { useState, useEffect, SyntheticEvent } from "react";
+import React, { useState, SyntheticEvent } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { useRecoilValue } from "recoil";
+import { add, sub } from "date-fns";
 import {
   Accordion,
   Strip,
@@ -9,86 +13,163 @@ import {
   Input,
 } from "@canonical/react-components";
 
-import CollaboratorsTable from "./CollaboratorsTable";
 import InvitesTable from "./InvitesTable";
+import { Invite } from "../types";
+
+import { inviteToRevokeState } from "../atoms";
 
 declare global {
   interface Window {
-    PACKAGE_NAME: string;
     CSRF_TOKEN: string;
   }
 }
 
 function Collaborators() {
+  const { packageName } = useParams();
+  const queryClient = useQueryClient();
+  const inviteToRevoke = useRecoilValue(inviteToRevokeState);
   const [showRevokeConfirmation, setShowRevokeConfirmation] = useState(false);
-  const [collaboratorToRevoke, setCollaboratorToRevoke] = useState("");
   const [showRevokeSuccess, setShowRevokeSuccess] = useState(false);
   const [showRevokeError, setShowRevokeError] = useState(false);
-  const [collaborators, setCollaborators] = useState([]);
-  const [invites, setInvites] = useState([]);
   const [newCollaboratorEmail, setNewCollaboratorEmail] = useState("");
   const [inviteLink, setInviteLink] = useState("");
   const [showAddCollaborator, setShowAddCollaborator] = useState(false);
-
-  const packageName = window?.PACKAGE_NAME;
 
   const closeRevokeConfirmation = () => {
     setShowRevokeConfirmation(false);
   };
 
-  const revokeInvite = (email: string) => {
-    const formData = new FormData();
+  const {
+    isLoading: invitesIsLoading,
+    isError: invitesIsError,
+    data: invites,
+  } = useQuery("invitesData", async () => {
+    const response = await fetch(`/${packageName}/invites`, {
+      cache: "no-cache",
+    });
 
-    formData.set("csrf_token", window?.CSRF_TOKEN);
-    formData.set("collaborator", email);
+    if (!response.ok) {
+      throw new Error("There was a problem fetching invites");
+    }
 
-    fetch(`/${packageName}/invites/revoke`, {
-      method: "POST",
-      body: formData,
-    }).then((response) => {
-      if (response.status === 200) {
-        // setCollaborators();
-        // collaborators.filter((collaborator) => collaborator?.email !== email)
+    const invitesData = await response.json();
+
+    return invitesData.invites;
+  });
+
+  const sendInviteMutation = useMutation(
+    async () => {
+      const formData = new FormData();
+
+      formData.set("collaborators", newCollaboratorEmail);
+      formData.set("csrf_token", window.CSRF_TOKEN);
+
+      const response = await fetch(`/${packageName}/invite`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("There was a problem sending invites");
+      }
+
+      const inviteData = await response.json();
+
+      setInviteLink(
+        `/${packageName}/collaboration/confirm?token=${inviteData?.result?.tokens?.[0]?.token}`
+      );
+    },
+    {
+      onMutate: async (inviteEmail: string) => {
+        await queryClient.cancelQueries("invitesData");
+
+        const previousInvites = queryClient.getQueryData("invitesData");
+        const newInvite = {
+          accepted_at: null,
+          accepted_by: null,
+          created_at: new Date().toISOString(),
+          created_by: "",
+          email: inviteEmail,
+          expires_at: sub(add(new Date(), { months: 1 }), {
+            days: 1,
+          }).toISOString(),
+          invite_type: "",
+          revoked_at: null,
+          revoked_by: null,
+        };
+
+        queryClient.setQueryData("invitesData", (oldInvites: any) => {
+          return [newInvite, ...oldInvites];
+        });
+
+        return { previousInvites };
+      },
+      onError: ({ context }) => {
+        queryClient.setQueryData("invitesData", context?.previousInvites);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries();
+      },
+    }
+  );
+
+  const revokeInviteMutation = useMutation(
+    async () => {
+      const formData = new FormData();
+
+      formData.set("csrf_token", window?.CSRF_TOKEN);
+      formData.set("collaborator", inviteToRevoke);
+
+      const response = await fetch(`/${packageName}/invites/revoke`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("There was a problem revoking this invite");
+      }
+
+      const responseData = await response.json();
+
+      if (responseData.success) {
         setShowRevokeSuccess(true);
       } else {
         setShowRevokeError(true);
       }
-    });
-  };
+    },
+    {
+      onMutate: async (inviteEmail: string) => {
+        await queryClient.cancelQueries("invitesData");
 
-  useEffect(() => {
-    fetch(`/${packageName}/invites`)
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        }
-      })
-      .then((data) => {
-        // setInvites(data?.invites);
-      });
-  }, [collaborators]);
+        const previousInvites: any = queryClient.getQueryData("invitesData");
 
-  const inviteCollaborator = (email?: string) => {
-    const formData = new FormData();
+        queryClient.setQueryData("invitesData", (oldInvites: any) => {
+          const revokedInvite = oldInvites.find(
+            (invite: Invite) => invite.email === inviteEmail
+          );
 
-    formData.set("collaborators", email || newCollaboratorEmail);
-    formData.set("csrf_token", window.CSRF_TOKEN);
+          revokedInvite.revoked_at = sub(new Date(), {
+            minutes: 1,
+          }).toISOString();
 
-    fetch(`/${packageName}/invite`, {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        }
-      })
-      .then((data) => {
-        setInviteLink(
-          `https://charmhub.io/${packageName}/collaboration/confirm?token=${data?.result?.tokens?.[0]?.token}`
-        );
-      });
-  };
+          return [
+            ...oldInvites.filter(
+              (invite: Invite) => invite.email !== inviteEmail
+            ),
+            revokedInvite,
+          ];
+        });
+
+        return { previousInvites };
+      },
+      onError: ({ context }) => {
+        queryClient.setQueryData("invitesData", context?.previousInvites);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries();
+      },
+    }
+  );
 
   return (
     <div className="l-application collaboration-ui">
@@ -101,8 +182,7 @@ function Collaborators() {
                 setShowRevokeSuccess(false);
               }}
             >
-              The invite for <strong>{collaboratorToRevoke}</strong> has been
-              revoked.
+              The invite for <strong>{inviteToRevoke}</strong> has been revoked.
             </Notification>
           )}
 
@@ -114,7 +194,7 @@ function Collaborators() {
               }}
             >
               There was a problem revoking the invite for{" "}
-              <strong>{collaboratorToRevoke}</strong>.
+              <strong>{inviteToRevoke}</strong>.
             </Notification>
           )}
 
@@ -134,25 +214,22 @@ function Collaborators() {
             sections={[
               {
                 key: "collaborators-table",
-                title: `Active shares (${collaborators.length})`,
+                title: `Invites ${
+                  invites && invites.length > 0 ? `(${invites.length})` : ""
+                }`,
                 content: (
-                  <CollaboratorsTable
-                    collaborators={collaborators}
-                    setCollaboratorToRevoke={setCollaboratorToRevoke}
-                    setShowRevokeConfirmation={setShowRevokeConfirmation}
-                  />
-                ),
-              },
-              {
-                title: `Invites (${invites.length})`,
-                content: (
-                  <InvitesTable
-                    invites={invites}
-                    collaborators={collaborators}
-                    setCollaboratorToRevoke={setCollaboratorToRevoke}
-                    setShowRevokeConfirmation={setShowRevokeConfirmation}
-                    inviteCollaborator={inviteCollaborator}
-                  />
+                  <>
+                    {!invitesIsLoading &&
+                      !invitesIsError &&
+                      invites &&
+                      invites.length > 0 && (
+                        <InvitesTable
+                          invites={invites}
+                          setShowRevokeConfirmation={setShowRevokeConfirmation}
+                          inviteCollaborator={() => false}
+                        />
+                      )}
+                  </>
                 ),
               },
             ]}
@@ -180,13 +257,13 @@ function Collaborators() {
               <i className="p-icon--close">Close</i>
             </Button>
           </div>
-          <div className="panel__content">
-            <Form
-              onSubmit={(e) => {
-                e.preventDefault();
-                inviteCollaborator();
-              }}
-            >
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendInviteMutation.mutate(newCollaboratorEmail);
+            }}
+          >
+            <div className="panel__content">
               <Notification severity="caution" title="Role">
                 A collaborator is a store user that can have equal rights over a
                 particular package as the package publisher.
@@ -212,19 +289,13 @@ function Collaborators() {
                   setNewCollaboratorEmail(e.target.value);
                 }}
               />
-            </Form>
-          </div>
-          <div className="panel__footer">
-            <Button
-              type="submit"
-              appearance="positive"
-              onClick={() => {
-                inviteCollaborator();
-              }}
-            >
-              Add collaborator
-            </Button>
-          </div>
+            </div>
+            <div className="panel__footer">
+              <Button type="submit" appearance="positive">
+                Add collaborator
+              </Button>
+            </div>
+          </Form>
         </div>
       </aside>
 
@@ -245,7 +316,7 @@ function Collaborators() {
                 appearance="positive"
                 className="u-no-margin--bottom"
                 onClick={() => {
-                  revokeInvite(collaboratorToRevoke);
+                  revokeInviteMutation.mutate(inviteToRevoke);
                   setShowRevokeConfirmation(false);
                 }}
               >
@@ -256,7 +327,7 @@ function Collaborators() {
         >
           <p>
             Are you sure you want to revoke the invite for{" "}
-            <strong>{collaboratorToRevoke}</strong>?
+            <strong>{inviteToRevoke}</strong>?
           </p>
         </Modal>
       )}
