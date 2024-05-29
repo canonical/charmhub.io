4a54b41a1b554865ffdf037a9aa6bf435f13c158
LukeWH <luke@lukewh.com>
2024-05-29 11:45:10 +0100

    ci: tics coverage upload

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     url: "http://localhost.test",
     customExportConditions: [""],
   },
+  collectCoverage: true,
+  coverageReporters: ["html", "cobertura"],
+  coverageDirectory: "coverage/js",
   transformIgnorePatterns: ["node_modules/@canonical/(?!react-components)"],
   moduleNameMapper: {
     "\\.(scss|sass|css)$": "identity-obj-proxy",
