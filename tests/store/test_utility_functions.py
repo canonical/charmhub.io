b6c8b4e0c0ac0c089c1e8602148f8278584aa0ee
Seulkee Kang <seulkeekang@gmail.com>
2024-06-06 16:44:47 +0100

    fix: fix lint python in the test

--- tests/store/test_utility_functions.py
+++ tests/store/test_utility_functions.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-from unittest.mock import patch
 from webapp.app import app
 from webapp.store.views import extract_juju_version, process_contacts
 
@@ -11,15 +10,20 @@ class TestUtilityFunctions(TestCase):
 
     def test_extract_juju_version(self):
         self.assertEqual(extract_juju_version(["juju >= 2.0"]), ">= 2.0")
-        self.assertEqual(extract_juju_version(["some_other_assumption", "juju 3.0"]), "3.0")
+        self.assertEqual(
+            extract_juju_version(["some_other_assumption", "juju 3.0"]), "3.0"
+        )
         self.assertEqual(extract_juju_version(["another_assumption"]), None)
         self.assertEqual(extract_juju_version([]), None)
 
     def test_process_contacts(self):
-        contact_list = ["Canonical <canonical@example.com>", "mailto:contact_name <contact@example.com>"]
+        contact_list = [
+            "Canonical <canonical@example.com>",
+            "mailto:contact_name <contact@example.com>",
+        ]
         expected_output = [
             {"name": "Canonical", "email": "canonical@example.com"},
-            {"name": "contact_name", "email": "contact@example.com"}
+            {"name": "contact_name", "email": "contact@example.com"},
         ]
         self.assertEqual(process_contacts(contact_list), expected_output)
 
