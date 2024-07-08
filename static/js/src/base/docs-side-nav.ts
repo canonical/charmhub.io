9b35e2ce5cd6884286d2606638cac4f675ad0252
Abbie Sims <abbie.sims@canonical.com>
2024-07-08 16:47:38 +0200

    fix: Fix compiler warnings in /static/js/src/base

--- static/js/src/base/docs-side-nav.ts
+++ static/js/src/base/docs-side-nav.ts
@@ -5,41 +5,40 @@
   @param {HTMLElement} sideNavigation The side navigation element.
   @param {Boolean} show Whether to show or hide the drawer.
 */
-  function toggleDrawer(sideNavigation, show) {
+  function toggleDrawer(sideNavigation: HTMLElement, show: boolean) {
     const toggleButtonOutsideDrawer = sideNavigation.querySelector(
       ".p-side-navigation__toggle"
-    );
+    ) as HTMLElement | null;
     const toggleButtonInsideDrawer = sideNavigation.querySelector(
       ".p-side-navigation__toggle--in-drawer"
-    );
+    ) as HTMLElement | null;
 
     if (sideNavigation) {
       if (show) {
         sideNavigation.classList.remove("is-drawer-collapsed");
         sideNavigation.classList.add("is-drawer-expanded");
 
-        toggleButtonInsideDrawer.focus();
-        toggleButtonOutsideDrawer.setAttribute("aria-expanded", true);
-        toggleButtonInsideDrawer.setAttribute("aria-expanded", true);
+        toggleButtonInsideDrawer?.focus();
+        toggleButtonOutsideDrawer?.setAttribute("aria-expanded", "true");
+        toggleButtonInsideDrawer?.setAttribute("aria-expanded", "true");
       } else {
         sideNavigation.classList.remove("is-drawer-expanded");
         sideNavigation.classList.add("is-drawer-collapsed");
 
-        toggleButtonOutsideDrawer.focus();
-        toggleButtonOutsideDrawer.setAttribute("aria-expanded", false);
-        toggleButtonInsideDrawer.setAttribute("aria-expanded", false);
+        toggleButtonOutsideDrawer?.focus();
+        toggleButtonOutsideDrawer?.setAttribute("aria-expanded", "false");
+        toggleButtonInsideDrawer?.setAttribute("aria-expanded", "false");
       }
     }
   }
 
   // throttle util (for window resize event)
-  var throttle = function (fn, delay) {
-    var timer = null;
-    return function () {
-      var context = this,
-        args = arguments;
-      clearTimeout(timer);
-      timer = setTimeout(function () {
+  const throttle = (fn: Function, delay: number) => {
+    let timer: number | null = null;
+    return function (this: any, ...args: any[]) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = window.setTimeout(() => {
         fn.apply(context, args);
       }, delay);
     };
@@ -49,31 +48,33 @@
     Attaches event listeners for the side navigation toggles
     @param {HTMLElement} sideNavigation The side navigation element.
   */
-  function setupSideNavigation(sideNavigation) {
-    var toggles = [].slice.call(
+  function setupSideNavigation(sideNavigation: HTMLElement) {
+    const toggles = Array.from(
       sideNavigation.querySelectorAll(".js-drawer-toggle")
-    );
-    var drawerEl = sideNavigation.querySelector(".p-side-navigation__drawer");
+    ) as HTMLElement[];
+    const drawerEl = sideNavigation.querySelector(
+      ".p-side-navigation__drawer"
+    ) as HTMLElement | null;
 
     // hide navigation drawer on small screens
     sideNavigation.classList.add("is-drawer-hidden");
 
     // setup drawer element
-    drawerEl.addEventListener("animationend", () => {
+    drawerEl?.addEventListener("animationend", () => {
       if (!sideNavigation.classList.contains("is-drawer-expanded")) {
         sideNavigation.classList.add("is-drawer-hidden");
       }
     });
 
-    window.addEventListener("keydown", (e) => {
+    window.addEventListener("keydown", (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         toggleDrawer(sideNavigation, false);
       }
     });
 
     // setup toggle buttons
-    toggles.forEach(function (toggle) {
-      toggle.addEventListener("click", function (event) {
+    toggles.forEach((toggle) => {
+      (toggle as HTMLElement).addEventListener("click", (event: MouseEvent) => {
         event.preventDefault();
 
         if (sideNavigation) {
@@ -89,9 +90,9 @@
     // hide side navigation drawer when screen is resized
     window.addEventListener(
       "resize",
-      throttle(function () {
-        toggles.forEach((toggle) => {
-          return toggle.setAttribute("aria-expanded", false);
+      throttle(() => {
+        toggles.forEach((toggle: Element) => {
+          return toggle.setAttribute("aria-expanded", "false");
         });
         // remove expanded/collapsed class names to avoid unexpected animations
         sideNavigation.classList.remove("is-drawer-expanded");
@@ -105,11 +106,11 @@
     Attaches event listeners for all the side navigations in the document.
     @param {String} sideNavigationSelector The CSS selector matching side navigation elements.
   */
-  function setupSideNavigations(sideNavigationSelector) {
+  function setupSideNavigations(sideNavigationSelector: string) {
     // Setup all side navigations on the page.
-    var sideNavigations = [].slice.call(
+    const sideNavigations = Array.from(
       document.querySelectorAll(sideNavigationSelector)
-    );
+    ) as HTMLElement[];
 
     sideNavigations.forEach(setupSideNavigation);
   }
@@ -118,36 +119,40 @@
 
   // Setup expandable side navigation
 
-  var expandToggles = document.querySelectorAll(".p-side-navigation__expand");
+  const expandToggles = document.querySelectorAll(
+    ".p-side-navigation__expand"
+  ) as NodeListOf<HTMLElement>;
 
   // setup default values of aria-expanded for the toggle button, list title and list itself
-  const setup = (toggle) => {
+  const setup = (toggle: HTMLElement) => {
     const isExpanded = toggle.getAttribute("aria-expanded") === "true";
     if (!isExpanded) {
-      toggle.setAttribute("aria-expanded", isExpanded);
+      toggle.setAttribute("aria-expanded", "false");
     }
-    const item = toggle.closest(".p-side-navigation__item");
-    const link = item.querySelector(".p-side-navigation__link");
-    const nestedList = item.querySelector(".p-side-navigation__list");
+    const item = toggle.closest(".p-side-navigation__item") as HTMLElement | null;
+    const link = item?.querySelector(".p-side-navigation__link") as HTMLElement| null;
+    const nestedList = item?.querySelector(".p-side-navigation__list") as HTMLElement | null;
     if (!link?.hasAttribute("aria-expanded")) {
-      link.setAttribute("aria-expanded", isExpanded);
+      link?.setAttribute("aria-expanded", isExpanded.toString());
     }
     if (!nestedList?.hasAttribute("aria-expanded")) {
-      nestedList.setAttribute("aria-expanded", isExpanded);
+      nestedList?.setAttribute("aria-expanded", isExpanded.toString());
     }
   };
 
-  const handleToggle = (e) => {
-    const item = e.currentTarget.closest(".p-side-navigation__item");
-    const button = item.querySelector(".p-side-navigation__expand");
-    const link = item.querySelector(".p-side-navigation__link");
-    const nestedList = item.querySelector(".p-side-navigation__list");
-    [button, link, nestedList].forEach((el) =>
-      el.setAttribute(
-        "aria-expanded",
-        el.getAttribute("aria-expanded") === "true" ? "false" : "true"
-      )
-    );
+  const handleToggle = (e: Event) => {
+    const item = (e.currentTarget as HTMLElement).closest(".p-side-navigation__item") as HTMLElement | null;
+    const button = item?.querySelector(".p-side-navigation__expand") as HTMLElement | null;
+    const link = item?.querySelector(".p-side-navigation__link") as HTMLElement | null;
+    const nestedList = item?.querySelector(".p-side-navigation__list") as HTMLElement | null;
+    [button, link, nestedList].forEach((el) => {
+      if (el) {
+        el.setAttribute(
+          "aria-expanded",
+          el.getAttribute("aria-expanded") === "true" ? "false" : "true"
+        );
+      }
+    });
   };
 
   expandToggles.forEach((toggle) => {
