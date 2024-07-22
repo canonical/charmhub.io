d85b479c2d2f288fb10f043ddfa45f7fe6cd4a10
Abbie Sims <74302970+abbiesims@users.noreply.github.com>
2024-07-22 11:48:42 +0200

    chore: increase code coverage interfaces/components/InterfaceDetailsNav (#1889)

---
+++ static/js/src/interfaces/components/InterfaceDetailsNav/__tests__/InterfaceDetailsNav.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import InterfaceDetailsNav from '../InterfaceDetailsNav';
+
+describe('InterfaceDetailsNav', () => {
+  test('renders Charms link', () => {
+    render(<InterfaceDetailsNav hasDeveloperDocumentation={false} />);
+
+    const charmsLink = screen.getByText('Charms');
+    expect(charmsLink).toBeInTheDocument();
+    expect(charmsLink).toHaveAttribute('href', '#charms');
+  });
+
+  test('renders Developer documentation link when hasDeveloperDocumentation is true', () => {
+    render(<InterfaceDetailsNav hasDeveloperDocumentation={true} />);
+
+    const devDocLink = screen.getByText('Developer documentation');
+    expect(devDocLink).toBeInTheDocument();
+    expect(devDocLink).toHaveAttribute('href', '#developer-documentation');
+  });
+
+  test('does not render Developer documentation link when hasDeveloperDocumentation is false', () => {
+    render(<InterfaceDetailsNav hasDeveloperDocumentation={false} />);
+
+    const devDocLink = screen.queryByText('Developer documentation');
+    expect(devDocLink).not.toBeInTheDocument();
+  });
+});
