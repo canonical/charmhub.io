8a45592217e1a53faa99d855e7790879590d22e5
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-08-06 10:54:54 +0300

    fix: fix tests

--- static/js/src/publisher-admin/pages/Listing/__tests__/Listing.test.tsx
+++ static/js/src/publisher-admin/pages/Listing/__tests__/Listing.test.tsx
@@ -131,12 +131,12 @@ describe("Listing", () => {
 
   test("disables 'Revert' button if no changes are made", () => {
     renderComponent(mockPackage);
-    expect(screen.getByRole("button", { name: "Revert" })).toBeDisabled();
+    expect(screen.getByRole("button", { name: "Revert" })).toHaveAttribute("aria-disabled", "true");
   });
 
   test("disables 'Save' button if no changes are made", () => {
     renderComponent(mockPackage);
-    expect(screen.getByRole("button", { name: "Save" })).toBeDisabled();
+    expect(screen.getByRole("button", { name: "Save" })).toHaveAttribute("aria-disabled", "true");
   });
 
   test("updates form fields correctly when user types input", () => {
