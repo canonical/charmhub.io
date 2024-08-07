f8b3ff12040086feb3574cf38bb7d46231a5fe09
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-08-07 17:09:15 +0300

    fix: remove webapp_beta from rock

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 # canonicalwebteam.flask-base requires SECRET_KEY to be set, this must be done before importing the app
 os.environ["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
 
-from webapp_beta.app import app
+from webapp.app import app
