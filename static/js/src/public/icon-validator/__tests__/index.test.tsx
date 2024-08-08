f858f524bac222403535c79ef03e19b41fffd53a
Abbie Sims <74302970+abbiesims@users.noreply.github.com>
2024-08-08 18:07:56 +0400

    chore: fix failing tests and linting (#1961)

--- static/js/src/public/icon-validator/__tests__/index.test.tsx
+++ static/js/src/public/icon-validator/__tests__/index.test.tsx
@@ -1,33 +1,55 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import ReactDOM from "react-dom/client";
+import { cleanup, fireEvent, screen, waitFor } from "@testing-library/react";
 import App from "../index";
 import "@testing-library/jest-dom";
 
-jest.mock("react-dom", () => ({
-  ...jest.requireActual("react-dom"),
-  render: jest.fn(),
-}));
+const renderApp = () => {
+  const container = document.createElement("div");
+  container.id = "main-content";
+  document.body.appendChild(container);
+
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+
+  return root;
+};
 
 describe("App component", () => {
+  let root: ReactDOM.Root;
+
   beforeEach(() => {
-    render(<App />);
+    root = renderApp();
   });
 
-  test("renders the app with initial state", () => {
-    expect(screen.getByText("Charm Icon Validator")).toBeInTheDocument();
-    expect(screen.queryByText(/Checks are based on the/i)).toBeInTheDocument();
-    expect(screen.getByText("Charm Icon Specification")).toBeInTheDocument();
-    expect(
-      screen.getByText(
-        "Drag and drop, or use the file picker to choose your icon."
-      )
-    ).toBeInTheDocument();
+  afterEach(() => {
+    root.unmount();
+    cleanup();
+    document.body.innerHTML = "";
+  });
+
+  test("renders the app with initial state", async () => {
+    await waitFor(() => {
+      expect(screen.getByText("Charm Icon Validator")).toBeInTheDocument();
+      expect(screen.getByText(/Checks are based on the/i)).toBeInTheDocument();
+      expect(screen.getByText("Charm Icon Specification")).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "Drag and drop, or use the file picker to choose your icon."
+        )
+      ).toBeInTheDocument();
+    });
   });
 
   test("displays the notification based on the validity of the icon", async () => {
+    await waitFor(() => {
+      expect(screen.getByRole("presentation")).toBeInTheDocument();
+    });
+
     const fileInput = screen
       .getByRole("presentation")
       .querySelector('input[type="file"]') as HTMLInputElement;
+
     const file = new File(
       [
         `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
@@ -40,15 +62,23 @@ describe("App component", () => {
 
     fireEvent.change(fileInput, { target: { files: [file] } });
 
-    setTimeout(() => {
-      expect(screen.findByText("Valid icon uploaded")).toBeInTheDocument();
-    }, 0);
+    setTimeout(async () => {
+      expect(
+        await screen.findByText("Valid icon uploaded")
+      ).toBeInTheDocument();
+      expect(await screen.getByText("Charmhub.io preview")).toBeInTheDocument();
+    }, 1000);
   });
 
   test("validates the icon's size and shows error if incorrect", async () => {
+    await waitFor(() => {
+      expect(screen.getByRole("presentation")).toBeInTheDocument();
+    });
+
     const fileInput = screen
       .getByRole("presentation")
       .querySelector('input[type="file"]') as HTMLInputElement;
+
     const invalidSizeFile = new File(
       [
         `<svg width="150" height="150" xmlns="http://www.w3.org/2000/svg">
@@ -61,29 +91,10 @@ describe("App component", () => {
 
     fireEvent.change(fileInput, { target: { files: [invalidSizeFile] } });
 
-    setTimeout(() => {
-      expect(screen.findByText("Icon size is invalid")).toBeInTheDocument();
-    }, 0);
-  });
-
-  test("renders the Charmhub.io preview when the icon is valid", async () => {
-    const fileInput = screen
-      .getByRole("presentation")
-      .querySelector('input[type="file"]') as HTMLInputElement;
-    const file = new File(
-      [
-        `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-               <rect width="100" height="100" fill="green" />
-             </svg>`,
-      ],
-      "valid-icon.svg",
-      { type: "image/svg+xml" }
-    );
-
-    fireEvent.change(fileInput, { target: { files: [file] } });
-
-    setTimeout(() => {
-      expect(screen.findByText("Charmhub.io preview")).toBeInTheDocument();
-    }, 0);
+    setTimeout(async () => {
+      expect(
+        await screen.findByText("Icon size is invalid")
+      ).toBeInTheDocument();
+    }, 1000);
   });
 });
