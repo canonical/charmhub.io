7e6537abc555ce531afeb36240f307237f262e25
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2024-08-13 17:22:30 +0300

    chore: remove get in touch

--- static/js/src/base/base.js
+++ static/js/src/base/base.js
@@ -1,7 +1,6 @@
 import { cookiePolicy } from "@canonical/cookie-policy";
 import "./polyfills";
 import "./navigation";
-import "./dynamic-contact-form";
 import "./tooltip-icon-modal";
 
 cookiePolicy();
