b5e7b6ebecdddabba7f5bed141d4d4cc8c143862
Mohammad Iskandarany <43501040+M7mdisk@users.noreply.github.com>
2024-08-16 13:20:05 +0300

    chore: remove beta store and fix redirects (#1974)

--- webapp/config.py
+++ webapp/config.py
@@ -1,6 +1,6 @@
 import os
 
-APP_NAME = "charmhub.io"
+APP_NAME = "charmhub"
 DETAILS_VIEW_REGEX = "[A-Za-z0-9-]*[A-Za-z][A-Za-z0-9-]*"
 
 CATEGORIES = [
--- webapp_beta/charmhub_bp.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-charmhub_bp = Blueprint(
-    "charmhub_bp",
-    __name__,
-    template_folder="../templates",
-    static_folder="../static",
-)
