01c75647b8d074ccab8532903aaf765a9682ba57
Mohammad Iskandarany <43501040+M7mdisk@users.noreply.github.com>
2024-08-28 16:50:50 +0300

    feat: charming charmhub (#1960)
    * Charming Charmhub - PaaS flask app (#1906)
    
    * chore: import app in app.py
    
    This is required for PaaS app charmer
    
    * chore: add .rock and .charm to gitignore
    
    * chore: create rockcraft.yaml
    
    using `rockcraft init --profile flask-framework`
    
    * chore: init charm (#1908)
    
    with `charmcraft init --profile flask-framework`
    
    * CI: add publish_charm workflow (#1909)
    
    * chore: use local registry
    
    * fix: remove webapp_beta from rock
    
    * chore: add charm readme
    
    * chore: add instructions to connect using browser
    
    * chore: add redirects to rockcraft.yaml
    
    * CI: attach resource to channel once uploaded
    
    * chore: update description
    
    * chore: split files
    
    * chore: update description
    
    * chore: remove experimental flags
    
    * fix: default maintenance flag to false (for now)
    
    * fix: change branch to main
    
    * chore: update readme
    
    * chore: add security.txt and robots.txt to rock
    
    * chore: better documentation
    
    * chore: pin paas-app-charmer
    
    * chore: add lib to .gitignore

---
+++ charm/src/charm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Flask Charm entrypoint."""
+
+import logging
+import typing
+
+import ops
+
+import paas_app_charmer.flask
+
+logger = logging.getLogger(__name__)
+
+
+class FlaskCharm(paas_app_charmer.flask.Charm):
+    """Flask Charm service."""
+
+    def __init__(self, *args: typing.Any) -> None:
+        """Initialize the instance.
+
+        Args:
+            args: passthrough to CharmBase.
+        """
+        super().__init__(*args)
+
+
+if __name__ == "__main__":
+    ops.main.main(FlaskCharm)
