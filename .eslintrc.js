d335c89082134299d5436b146c8e1bd0a73b2c6b
Seulkee Kang <seulkeekang@gmail.com>
2024-11-12 14:52:04 +0100

    chore: remove overrides from the config

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,20 +39,4 @@ module.exports = {
       version: "detect",
     },
   },
-  overrides: [
-    {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
-      rules: {
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-types": "off",
-        "prefer-const": "off",
-        "react/no-children-prop": "off",
-        "no-sparse-arrays": "off",
-      },
-    },
-  ],
 };
