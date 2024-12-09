d91999f36b3bbd3b9ea79d733e4f670b3cb79cde
Abbie Sims <abbie.sims@canonical.com>
2024-12-09 15:34:55 +0000

    fix: highlight active item on interfaces side nav

--- static/js/src/interfaces/components/InterfaceDetailsNav/InterfaceDetailsNav.tsx
+++ static/js/src/interfaces/components/InterfaceDetailsNav/InterfaceDetailsNav.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 type Props = {
   hasDeveloperDocumentation: boolean;
 };
 
 function InterfaceDetailsNav({ hasDeveloperDocumentation }: Props) {
+  const [activeLink, setActiveLink] = useState("charms");
   return (
     <div className="p-side-navigation">
       <ul className="p-side-navigation__list">
         <li className="p-side-navigation__item">
-          <a href="#charms" className="p-side-navigation__link is-active">
+          <a
+            href="#charms"
+            className={`p-side-navigation__link ${
+              activeLink === "charms" ? "is-active" : ""
+            }`}
+            onClick={() => setActiveLink("charms")}
+          >
             Charms
           </a>
         </li>
@@ -15,7 +24,10 @@ function InterfaceDetailsNav({ hasDeveloperDocumentation }: Props) {
           <li className="p-side-navigation__item">
             <a
               href="#developer-documentation"
-              className="p-side-navigation__link"
+              className={`p-side-navigation__link ${
+                activeLink === "developer-documentation" ? "is-active" : ""
+              }`}
+              onClick={() => setActiveLink("developer-documentation")}
             >
               Developer documentation
             </a>
