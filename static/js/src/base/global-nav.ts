d18be82ee4a004dfe94925dfbdefd0e96e0adb48
Bartek Szopka <bartek.szopka@canonical.com>
2025-01-29 17:23:14 +0100

    Readjust the breakpoint to fit a signed in user name

--- static/js/src/base/global-nav.ts
+++ static/js/src/base/global-nav.ts
@@ -1,5 +1,5 @@
 import { createNav } from "@canonical/global-nav";
 
 createNav({
-  breakpoint: 1110, // keep it in sync with $breakpoint-navigation-threshold in styles.scss
+  breakpoint: 1220, // keep it in sync with $breakpoint-navigation-threshold in styles.scss
 });
