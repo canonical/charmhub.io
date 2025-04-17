d68f58d0fa0fb686262ad30453818fd92b25e00f
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2025-04-17 14:47:07 +0300

    fix: disable talisker for charmed deployment

--- app.py
+++ app.py
@@ -2,8 +2,12 @@
 # The flask application must be defined in this file under the variable name `app`.
 # See - https://documentation.ubuntu.com/rockcraft/en/latest/reference/extensions/flask-framework/
 import os
+import logging
 
 # canonicalwebteam.flask-base requires SECRET_KEY to be set, this must be done before importing the app
 os.environ["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
+
+# disable talisker logger, as it is not used in this application and clutters logs
+logging.getLogger("talisker.context").disabled = True
 
 from webapp.app import app
