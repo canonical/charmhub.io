7b43259361a931446540e2e9d2f68985b61994c4
Mohammad Iskandarany <43501040+M7mdisk@users.noreply.github.com>
2025-08-04 11:37:03 +0300

    chore: refactor interfaces to use new index.json (#2161)
    * refactor integrations
    
    * test: rewrite tests

--- webapp/integrations/logic.py
+++ webapp/integrations/logic.py
@@ -1,12 +1,13 @@
 import re
-import time
 from github import Github
 from os import getenv
-
-import requests
+from functools import lru_cache
 
 from webapp.helpers import get_yaml_loader
 from webapp.observability.utils import trace_function
+from webapp.packages.logic import (
+    get_package,
+)
 
 GITHUB_TOKEN = getenv("GITHUB_TOKEN")
 
@@ -15,78 +16,43 @@ yaml = get_yaml_loader()
 
 
 class Interfaces:
-    interfaces = []
-    last_fetch = None
-    repo = None
-
     def __init__(self):
-        self.repo = github_client.get_repo(
-            "canonical/charm-relation-interfaces"
-        )
+        self._repo = None
 
+    @property
+    def repo(self):
+        if self._repo is None:
+            self._repo = github_client.get_repo(
+                "canonical/charm-relation-interfaces"
+            )
+        return self._repo
+
+    @lru_cache(maxsize=None)
     @trace_function
     def get_interfaces(self):
-        if (
-            len(self.interfaces) == 0
-            or not self.last_fetch
-            or time.time() - self.last_fetch > 172800
-        ):
-            interfaces_contents = self.repo.get_contents("interfaces")
-            interfaces_table = []
-
-            for interface in interfaces_contents:
-                if interface.name.startswith("__"):
-                    continue
-                interface_content = self.repo.get_contents(
-                    f"interfaces/{interface.name}"
-                )
-                for version in interface_content:
-                    interface_details = {}
-                    version_content = self.repo.get_contents(
-                        f"interfaces/{interface.name}/{version.name}"
-                    )
-                    for details in version_content:
-                        if details.name == "interface.yaml":
-                            interface_yaml = yaml.load(
-                                details.decoded_content.decode("utf-8")
-                            )
-                            interface_details["name"] = interface_yaml.get(
-                                "name", interface.name
-                            )
-
-                            if interface_yaml.get("status") == "published":
-                                interface_details["status"] = "live"
-                            elif interface_yaml.get("status") == "draft":
-                                interface_details["status"] = "draft"
-                            interface_details["version"] = interface_yaml.get(
-                                "version", version.name[1:]
-                            )
-                        if details.name == "README.md":
-                            interface_details["readme_path"] = details.html_url
-                    if interface_details.get("status", "") in [
-                        "live",
-                        "draft",
-                    ]:
-                        interfaces_table.append(interface_details)
-            self.interfaces = interfaces_table
-            self.last_fetch = time.time()
-        return self.interfaces
-
-    @trace_function
-    def repo_has_interface(self, interface):
         try:
-            self.repo.get_contents("interfaces/{}".format(interface))
-            return True
+            index = self.repo.get_contents("index.json")
+            if isinstance(index, list):
+                index = index[0]
+            index_content = index.decoded_content.decode("utf-8")
+            interfaces = yaml.load(index_content)
+            return interfaces
         except Exception:
-            return False
+            return []
 
+    @lru_cache(maxsize=None)
     @trace_function
     def get_interface_from_path(self, interface_name):
-        interface_versions = self.repo.get_contents(
-            "interfaces/{}".format(interface_name)
-        )
+        try:
+            interface_versions = self.repo.get_contents(
+                "interfaces/{}".format(interface_name)
+            )
+        except Exception:
+            return None
         versions = []
-        for i, version in enumerate(interface_versions):
+        if not isinstance(interface_versions, list):
+            interface_versions = [interface_versions]
+        for _, version in enumerate(interface_versions):
             if version.type == "dir" and version.name.startswith("v"):
                 versions.append(version.name)
 
@@ -103,26 +69,26 @@ class Interfaces:
 
         active_providers = []
         active_requirers = []
-        url = "https://charmhub.io/"
+
         if "providers" in interface and interface["providers"]:
+            active_providers = []
             for provider in interface["providers"]:
                 try:
-                    p = requests.get(f"{url}/{provider['name']}")
-                    if p.status_code != 404:
-                        active_providers.append(provider)
+                    get_package(provider["name"], [], False)
+                    active_providers.append(provider)
                 except Exception:
                     continue
+            interface["providers"] = active_providers
 
-                interface["providers"] = active_providers
         if "requirers" in interface and interface["requirers"]:
+            active_requirers = []
             for requirer in interface["requirers"]:
                 try:
-                    c = requests.get(f"{url}/{requirer['name']}")
-                    if c.status_code != 404:
-                        active_requirers.append(requirer)
+                    get_package(requirer["name"], [], False)
+                    active_requirers.append(requirer)
                 except Exception:
                     continue
-                interface["requirers"] = active_requirers
+            interface["requirers"] = active_requirers
 
         return interface
 
@@ -227,3 +193,6 @@ class Interfaces:
     def get_interface_name_from_readme(self, text):
         name = re.sub(r"[#` \n]", "", text.split("\n##", 1)[0]).split("/")[0]
         return name
+
+
+interface_logic = Interfaces()
--- webapp/integrations/views.py
+++ webapp/integrations/views.py
@@ -2,20 +2,14 @@ from datetime import datetime
 from flask import (
     Blueprint,
     render_template,
-    make_response,
-    redirect,
     abort,
-    url_for,
 )
 from flask.json import jsonify
-from github import Github
-from os import getenv
 
-from webapp.integrations.logic import Interfaces
+from webapp.integrations.logic import interface_logic
 from webapp.observability.utils import trace_function
 from webapp.store_api import publisher_gateway
 
-interface_logic = Interfaces()
 
 integrations = Blueprint(
     "integrations",
@@ -24,79 +18,29 @@ integrations = Blueprint(
     static_folder="/static",
 )
 
-GITHUB_TOKEN = getenv("GITHUB_TOKEN")
-
-github_client = Github(GITHUB_TOKEN)
-
 
 @trace_function
-def get_interfaces():
+@integrations.route("/integrations.json")
+def interfaces_json():
     interfaces = interface_logic.get_interfaces()
 
     response = {
         "interfaces": interfaces,
         "size": len(interfaces),
     }
+
     return response
 
 
 @trace_function
-@integrations.route("/integrations.json")
-def interfaces_json():
-    return get_interfaces()
-
-
-@trace_function
-@integrations.route("/integrations", defaults={"path": ""})
-def all_interfaces(path):
-    if not getenv("ENVIRONMENT") in ["devel", "staging"]:
-        return render_template("404.html")
-
-    response = get_interfaces()
-    context = {"interfaces": response["interfaces"]}
+@integrations.route("/integrations")
+def all_interfaces():
+    interfaces = interface_logic.get_interfaces()
+    context = {"interfaces": interfaces}
     return render_template("interfaces/index.html", **context)
 
 
-@trace_function
-@integrations.route("/integrations/<path:path>")
-def single_interface(path):
-    is_draft = path.endswith("draft")
-    interface = None
-
-    if is_draft:
-        response = get_single_interface(path.replace("/draft", ""), "draft")
-    else:
-        response = get_single_interface(path, "")
-
-    interface = eval(response.data.decode("utf-8"))
-
-    if (
-        "status" not in interface
-        and "other_charms" in interface
-        and len(interface["other_charms"]["providers"]) == 0
-        and len(interface["other_charms"]["requirers"]) == 0
-    ):
-        return abort(404)
-
-    if (
-        not is_draft
-        and "status" in interface
-        and interface["status"] == "draft"
-    ):
-        return redirect(url_for(".single_interface", path=f"{path}/draft"))
-
-    context = {"interface": interface}
-    return render_template("interfaces/index.html", **context)
-
-
-@trace_function
-@integrations.route(
-    "/integrations/<interface_name>.json", defaults={"status": ""}
-)
-@integrations.route("/integrations/<interface_name>/<status>.json")
-def get_single_interface(interface_name, status):
-    repo_has_interface = interface_logic.repo_has_interface(interface_name)
-
+def fetch_interface_details(interface_name):
     other_requirers = publisher_gateway.find(requires=[interface_name]).get(
         "results", []
     )
@@ -104,45 +48,65 @@ def get_single_interface(interface_name, status):
         "results", []
     )
 
-    res = {}
-    # check if interface exists in github repo
-    if not repo_has_interface:
-        res["other_charms"] = {
+    res = {
+        "other_charms": {
             "providers": other_providers,
             "requirers": other_requirers,
         }
-        return jsonify(res)
+    }
 
     interface = interface_logic.get_interface_from_path(interface_name)
+    if interface is None:
+        if other_providers or other_requirers:
+            return res
+        return None
 
-    readme_contentfile = interface_logic.repo.get_contents(
+    readme_path = (
         f"interfaces/{interface_name}/v{interface['version']}/README.md"
     )
+    readme_contentfile = interface_logic.repo.get_contents(readme_path)
     readme = readme_contentfile.decoded_content.decode("utf-8")
-
     last_modified = datetime.strptime(
         readme_contentfile.last_modified, "%a, %d %b %Y %H:%M:%S %Z"
     ).isoformat()
 
-    res["body"] = interface_logic.convert_readme(
-        interface_name, f"v{interface['version']}", readme, 2
+    res.update(
+        {
+            "body": interface_logic.convert_readme(
+                interface_name, f"v{interface['version']}", readme, 2
+            ),
+            "name": interface_logic.get_interface_name_from_readme(readme),
+            "charms": {
+                "providers": interface["providers"],
+                "requirers": interface["requirers"],
+            },
+            "last_modified": last_modified,
+            "status": interface["status"],
+            "version": interface["version"],
+        }
     )
 
-    res["name"] = interface_logic.get_interface_name_from_readme(readme)
-    res["charms"] = {
-        "providers": interface["providers"],
-        "requirers": interface["requirers"],
-    }
-    res["last_modified"] = last_modified
-    res["status"] = interface["status"]
+    return res
 
-    res["version"] = interface["version"]
 
-    res["other_charms"] = {
-        "providers": other_providers,
-        "requirers": other_requirers,
-    }
+@trace_function
+@integrations.route("/integrations/<path:path>")
+def get_single_interface(path):
+    interface_data = fetch_interface_details(path)
 
-    response = make_response(res)
-    response.cache_control.max_age = "76800"
-    return response
+    if interface_data is None:
+        return abort(404)
+
+    context = {"interface": interface_data}
+    return render_template("interfaces/index.html", **context)
+
+
+@trace_function
+@integrations.route("/integrations/<interface_name>.json")
+def get_single_interface_json(interface_name):
+    interface_data = fetch_interface_details(interface_name)
+
+    if interface_data is None:
+        return jsonify({"error": "Interface not found"}), 404
+
+    return jsonify(interface_data)
