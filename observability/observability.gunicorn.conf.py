f428fb4c60a0bf9516c9c9985895fc5b28e7e97f
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2025-08-20 13:19:58 +0300

    add loki and promtail for logs

--- observability/observability.gunicorn.conf.py
+++ observability/observability.gunicorn.conf.py
@@ -1,8 +1,10 @@
+from gevent import monkey
+monkey.patch_all()
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({x-request-id}o)s'
 
 def post_fork(server, worker):
     trace.set_tracer_provider(TracerProvider())
