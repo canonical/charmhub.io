e4cdc965b7e18bd2ef0e4be7dfedf27c33520121
Alvaro Mateo <alvaromateo9@gmail.com>
2025-10-17 11:37:58 +0200

    WD-30030 - Fix dropdowns not working

--- static/js/src/base/base.js
+++ static/js/src/base/base.js
@@ -1,6 +1,7 @@
 import { cookiePolicy } from "@canonical/cookie-policy";
 import "./polyfills";
-import "./navigation";
 import "./tooltip-icon-modal";
+// To be re-imported once the changes from global-nav have been undone (WD-29514)
+// import "./navigation";
 
 cookiePolicy();
