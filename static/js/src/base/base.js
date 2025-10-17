90d0737af163de3d5cb349296650ed27559a5206
Alvaro Mateo <alvaromateo9@gmail.com>
2025-10-17 12:00:47 +0200

    Revert "WD-30030 - Fix dropdowns not working"
    This reverts commit e4cdc965b7e18bd2ef0e4be7dfedf27c33520121.

--- static/js/src/base/base.js
+++ static/js/src/base/base.js
@@ -1,7 +1,6 @@
 import { cookiePolicy } from "@canonical/cookie-policy";
 import "./polyfills";
+import "./navigation";
 import "./tooltip-icon-modal";
-// To be re-imported once the changes from global-nav have been undone (WD-29514)
-// import "./navigation";
 
 cookiePolicy();
