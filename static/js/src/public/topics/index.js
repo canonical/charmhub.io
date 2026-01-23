d30acea84c03b825ff12d2a09a5449b1e6715fa8
Eduard M <4310497+edisile@users.noreply.github.com>
2026-01-23 10:52:59 +0100

    chore: replace implicit expose-loader with explicit declareGlobal calls

--- static/js/src/public/topics/index.js
+++ static/js/src/public/topics/index.js
@@ -1,3 +1,6 @@
 import { initTopicFilters } from "./filter";
+import declareGlobal from "../../libs/declare";
 
 export { initTopicFilters };
+
+declareGlobal("charmhub.topics", { initTopicFilters });
