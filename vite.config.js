f6d082cdd004e654a78dcf17cc3e9a3c8fe574e2
Eduard M <4310497+edisile@users.noreply.github.com>
2026-01-28 18:25:42 +0100

    fix: bundle prism js language styles

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import autoprefixer from "autoprefixer";
+import prismjsPlugin from "vite-plugin-prismjs-plus";
 import vitePluginDetectInput from "./vitePluginDetectInput";
 
 const env = loadEnv("all", process.cwd());
@@ -10,6 +11,9 @@ export default defineConfig({
     vitePluginDetectInput({
       regex: /vite_import\(["'](.+)["']\)/g,
       glob: "./templates/**/*.html",
+    }),
+    prismjsPlugin({
+      languages: ["python", "bash", "shell"],
     }),
     react(),
   ],
