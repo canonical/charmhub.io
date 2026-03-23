7f0c1c855242a9e3fb50da8d4e5ee96f4c780352
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2026-03-23 19:10:58 +0300

    fail import safely

--- terraform/demo/juju_imports.sh
+++ terraform/demo/juju_imports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Import the charm deployed via juju CLI into Terraform state.
 # DEMO_ID and MODEL_UUID are provided by the action as environment variables.
-terraform -chdir=terraform/demo import juju_application.demo "${MODEL_UUID}:${DEMO_ID}"
+terraform -chdir=terraform/demo import juju_application.demo "${MODEL_UUID}:${DEMO_ID}" || true
 
