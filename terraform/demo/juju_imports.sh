953ae9498566b1ea037f81e00dc354e62b749c75
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2026-03-24 11:27:45 +0300

    remove import script

--- terraform/demo/juju_imports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Import the charm deployed via juju CLI into Terraform state.
-# DEMO_ID and MODEL_UUID are provided by the action as environment variables.
-if ! terraform -chdir=terraform/demo state show juju_application.demo >/dev/null 2>&1; then
-  terraform -chdir=terraform/demo import juju_application.demo "${MODEL_UUID}:${DEMO_ID}"
-fi
